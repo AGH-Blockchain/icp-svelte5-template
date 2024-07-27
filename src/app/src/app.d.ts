ee00a28d3aa876bb8f63b8e650e89da2458d9b38
Mateusz Zając <matzayonc@gmail.com>
2024-07-27 15:04:45 +0200

    added sveltekit typing

--- src/app/src/app.d.ts
+++ src/app/src/app.d.ts
@@ -1,3 +1,4 @@
+/// <reference types="@sveltejs/kit" /> 
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare global {
@@ -10,4 +11,4 @@ declare global {
 	}
 }
 
-export {};
+export { };
